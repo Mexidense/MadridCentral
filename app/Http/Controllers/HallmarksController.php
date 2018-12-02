c63d569b6cc27cd4a3c27f849d38aac9bf568f24
Mexidense <mexidense@gmail.com>
2018-12-02 20:41:08 +0100

    hallmark single created

--- app/Http/Controllers/HallmarksController.php
+++ app/Http/Controllers/HallmarksController.php
@@ -8,10 +8,50 @@ use Illuminate\Http\Request;
 class HallmarksController extends Controller
 {
     public function getTag($id){
-        $hallmark = Hallmark::findOrFail($id);
+        try{
+            $hallmark = Hallmark::find($id);
 
-        return view('hallmark.single')->with([
-           'hallmark' => $hallmark,
-        ]);
+            $hallmark->id = $id;
+            $hallmark->tagType = $this->tagType($hallmark->tag);
+
+            return view('hallmark.single')->with([
+                'hallmark' => $hallmark,
+            ]);
+
+        }catch (\Exception $e){
+            return redirect('/');
+        }
+    }
+
+    public function tagType($tag){
+        switch ($tag){
+            case '16T0':
+                return '0';
+                break;
+            case '16TE':
+                return 'ECO';
+                break;
+            case '16TC':
+                return 'C';
+                break;
+            case '16TB':
+                return 'B';
+                break;
+            case '16M0':
+                return '0';
+                break;
+            case '16ME':
+                return 'ECO';
+                break;
+            case '16MC':
+                return 'C';
+                break;
+            case '16MB':
+                return 'B';
+                break;
+            default:
+                return 'NOT';
+                break;
+        }
     }
 }
