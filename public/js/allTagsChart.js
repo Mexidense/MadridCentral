6d0104edefe3ed7a7832afbe04d89433f2963477
Salvador B <mexidense@gmail.com>
2018-12-09 20:25:58 +0100

    home modified

---
+++ public/js/allTagsChart.js
@@ -0,0 +1,45 @@
+    // Load google charts
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Etiqueta', 'Tipo', { role: 'style' }, { role: 'annotation' }],
+        ['Etiqueta 0 - Azul', 50203, 'color: blue', '0'],
+        ['Etiqueta ECO - Verde/Azul', 274752, 'color: green', 'ECO'],
+        ['Etiqueta C - Verde', 8519700, 'color: green', 'C'],
+        ['Etiqueta B - Amarilla', 11336460, 'color: yellow', 'B'],
+        ['Sin etiqueta', 12441842, 'color: black', 'Sin etiqueta']
+    ]);
+
+    // Optional; add a title and set the width and height of the chart
+    var options = {
+        chart: {
+            title:'Proporción etiquetas medioambientales',
+            subtitle: 'Fuente: Portal Estadístico de la DGT - Diciembre 2018'
+        },
+        chartArea: {width: '50%'},
+        vAxis: {
+            title: 'Tipo de etiqueta'
+        },
+        hAxis: {
+            title: 'Total de vehículos',
+            minValue: 0,
+        },
+        bars: 'horizontal',
+        bar: { groupWidth: "90%" }
+
+    };
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+        { calc: "stringify",
+            sourceColumn: 1,
+            type: "string",
+            role: "annotation" },
+        2]);
+
+    var chart = new google.charts.Bar(document.getElementById('allTagsChart'));
+    chart.draw(view, options);
+}
